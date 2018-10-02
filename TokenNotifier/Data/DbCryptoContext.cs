078ad41e0f1f6995be5f238c76699e213a477540
Anton Pavlutsky <warger1@yandex.ru>
2018-10-02 19:22:46 +0300

    Добавьте файлы проекта.

---
+++ TokenNotifier/Data/DbCryptoContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TokenNotifier.Models;
+
+namespace TokenNotifier.Data
+{
+    public class DbCryptoContext : DbContext
+    {
+        public DbCryptoContext(DbContextOptions<DbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Notification>().ToTable("Notification");
+            modelBuilder.Entity<Wallet>().ToTable("Wallet");
+            modelBuilder.Entity<Token>().ToTable("Token");
+        }
+    }
+}
